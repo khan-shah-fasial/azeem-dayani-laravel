74e5274272e89dd4609ecce27ca5c5a16d3100f9
Faisal <khanfasial.makent@gmail.com>
2024-10-05 19:23:33 +0530

    changes in header

--- resources/views/backend/partials/header.blade.php
+++ resources/views/backend/partials/header.blade.php
@@ -372,7 +372,7 @@
                         <li class="dropdown">
                             <a class="nav-link dropdown-toggle arrow-none nav-user px-2" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
                                 <span class="account-user-avatar">
-                                    <img src="/assets/images/avatar-1.jpg" alt="user-image" width="32" class="rounded-circle">
+                                    {{-- <img src="/assets/images/avatar-1.jpg" alt="user-image" width="32" class="rounded-circle"> --}}
                                 </span>
                                 <span class="d-lg-flex flex-column gap-1 d-none">
                                     <h5 class="my-0">{{ auth()->user()->name }}</h5>
